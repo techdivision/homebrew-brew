8a4335ba0a8178d802f715990e04000bfcb376f9
Johann Zelger <j.zelger@techdivision.com>
2019-02-14 18:38:02 +0100

    added bottle for php@5.6

--- Formula/php@5.6.rb
+++ Formula/php@5.6.rb
@@ -5,10 +5,10 @@ class PhpAT56 < Formula
   sha256 "8147576001a832ff3d03cb2980caa2d6b584a10624f87ac459fcd3948c6e4a10"
 
   bottle do
-    sha256 "0f616518cdc1b20b0356ca22e7dd77a69da5c4f6354932868bd8ed3196f04872" => :mojave
-    sha256 "1d19ae0c315760f6d1cb97bc6437eaa6b1413f0cd022b93810c65eb62b4a56bb" => :high_sierra
-    sha256 "30a6ca5cdfd8ec6ad6e5c1f6f251248da3fff6f75d8916b1811ec5b3f8d17b0f" => :sierra
+    root_url "https://dl.bintray.com/techdivision/homebrew-brew/"
+    sha256 "fdfe900c9669adced0ec505ea56888e7f6def3325580e999564d73b8c01c5220" => :mojave
   end
+
 
   keg_only :versioned_formula
 
